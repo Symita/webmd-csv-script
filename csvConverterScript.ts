5fbecd0b1362f80062fa50265bc6a26bd41f7a7c
shravan meena <shravansymita@shravans-MacBook-Pro.local>
2024-03-14 00:39:59 +0530

    csv

--- csvConverterScript.ts
+++ csvConverterScript.ts
@@ -7,6 +7,7 @@ import { pipeline } from 'stream';
 import { promisify } from 'util';
 
 const streamPipeline = promisify(pipeline);
+
 // Update AWS config
 AWS.config.update({
     accessKeyId: 'AKIAQ7DJ4T4VGXDHQMLU',
@@ -14,57 +15,74 @@ AWS.config.update({
     region: 'us-east-1'
 });
 
-// Initialize S3 instance
+
 const s3 = new AWS.S3();
 
-/**
- * Uploads a file to AWS S3 and returns the URL of the uploaded file.
- * @param {string} filePath The path to the file to upload.
- * @param {string} mimeType The MIME type of the file.
- * @returns {Promise<string>} The URL of the uploaded file on S3.
- */
-async function uploadFile(filePath: string, mimeType: string): Promise<string> {
+
+async function downloadCsvFromUrl(url: string, outputPath: string): Promise<void> {
+    console.log(`Starting download of CSV file from ${url}`);
+
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream',
+    });
+
+    const totalLength = response.headers['content-length'];
+
+    let downloadedLength = 0;
+    let percentageCompleted = 0;
+
+    response.data.on('data', (chunk: Buffer) => {
+        downloadedLength += chunk.length;
+        percentageCompleted = (downloadedLength / parseInt(totalLength)) * 100;
+        process.stdout.write(`Downloading: ${percentageCompleted.toFixed(2)}% \r`);
+    });
+
+    await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
+    
+    const writer = fs.createWriteStream(outputPath);
+
+    response.data.pipe(writer);
+
     return new Promise((resolve, reject) => {
-        const fileStream = fs.createReadStream(filePath);
-        const bucketName = 'symita-webmd-media'; // Change to your bucket name
-        const objectKey = `${Date.now()}_${path.basename(filePath)}`;
-
-        const params = {
-            Bucket: bucketName,
-            Key: objectKey,
-            Body: fileStream,
-            ContentType: mimeType
-        };
-
-        s3.upload(params, (err: any, data: any) => {
-            if (err) {
-                console.error('Error uploading file:', err);
-                reject(err);
-            } else {
-                console.log('File uploaded successfully:', data.Location);
-                resolve(data.Location);
-            }
+        writer.on('finish', () => {
+            console.log(`\nCSV file downloaded successfully and saved to ${outputPath}`);
+            resolve();
         });
+        writer.on('error', reject);
     });
 }
 
-/**
- * Wrapper function for uploading images, setting the MIME type accordingly.
- * @param {string} imagePath Path to the image file.
- * @returns {Promise<string>} The URL of the uploaded image on S3.
- */
-export async function uploadImage(imagePath: any): Promise<string> {
-    const mimeType = 'image/jpeg'; // Adjust based on your image's format
-    console.log('Uploading started... ' + imagePath);
-    return uploadFile(imagePath, mimeType);
+
+async function uploadFile(filePath: string, mimeType: string): Promise<string> {
+    const fileStream = fs.createReadStream(filePath);
+    const bucketName = 'symita-webmd-media';
+    const objectKey = `${Date.now()}_${path.basename(filePath)}`;
+
+    const params = {
+        Bucket: bucketName,
+        Key: objectKey,
+        Body: fileStream,
+        ContentType: mimeType,
+    };
+
+    try {
+        const { Location } = await s3.upload(params).promise();
+        console.log('File uploaded successfully:', Location);
+        return Location;
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        return '';
+    }
 }
 
-/**
- * Wrapper function for uploading videos, setting the MIME type to 'video/mp4'.
- * @param {string} videoPath Path to the video file.
- * @returns {Promise<string>} The URL of the uploaded video on S3.
- */
-export async function uploadVideo(videoPath: any): Promise<string> {
+export async function uploadImage(imagePath: string): Promise<string> {
+    console.log('Uploading started... ' + imagePath);
+    return uploadFile(imagePath, 'image/jpeg');
+}
+
+export async function uploadVideo(videoPath: string): Promise<string> {
     return uploadFile(videoPath, 'video/mp4');
 }
 
@@ -74,7 +92,6 @@ async function downloadMediaWithAxios(url: string, outputPath: string) {
             url,
             method: 'GET',
             responseType: 'stream',
-            timeout: 0
         });
         console.log(`Download Started... ${url}`);
         await fs.promises.mkdir(path.dirname(outputPath), { recursive: true });
@@ -82,7 +99,6 @@ async function downloadMediaWithAxios(url: string, outputPath: string) {
         console.log(`Download completed and saved to ${outputPath}`);
     } catch (error) {
         console.error(`Failed to download: ${url}`, error);
-        throw error;
     }
 }
 
@@ -121,36 +137,39 @@ async function processMedia(row: any): Promise<void> {
     }
 }
 
-async function writeToJsonFile(filePath: string, data: any[]): Promise<void> {
-    fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
-        if (err) {
-            console.error('Failed to save JSON:', err);
-        } else {
-            console.log('CSV file has been processed and saved.');
-        }
-    });
-}
+
 
 async function uploadCsvToServer(): Promise<void> {
-    const csvFilePath = path.join(__dirname, './content_library_krames_dump.csv');
+    const csvUrl = 'https://symita-webmd-media.s3.amazonaws.com/content_library_krames_dump_full.csv';
+    const localCsvFilePath = path.join(__dirname, './content_library_krames_dump_full.csv');
     const jsonFilePath = path.join(__dirname, './output.json');
+
+    await downloadCsvFromUrl(csvUrl, localCsvFilePath); // Download CSV file
+
     let jsonData: any[] = [];
 
-    const rows: any = []; // Temporarily store rows to process them later
-    fs.createReadStream(csvFilePath)
+    fs.createReadStream(localCsvFilePath)
         .pipe(csv())
-        .on('data', (row) => {
-            rows.push(row); // Accumulate rows for sequential processing
+        .on('data', async (row) => {
+            try {
+                await processMedia(row);
+                jsonData.push(row);
+            } catch (error) {
+                console.error('Error processing row, continuing with next:', error);
+            }
         })
         .on('end', async () => {
-            for (let row of rows) {
-                await processMedia(row); // Process each row sequentially
-                jsonData.push(row); // Add the processed row to jsonData
-            }
-            // Write the modified jsonData to a new JSON file
-            await writeToJsonFile(jsonFilePath, jsonData);
-            console.log('CSV processing completed.');
+            fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), (err) => {
+                if (err) {
+                    console.error('Failed to save JSON:', err);
+                } else {
+                    console.log('CSV file has been processed and saved.');
+                }
+            });
         });
 }
 
-uploadCsvToServer();
+
+
+
+uploadCsvToServer().catch(console.error);
